a01aa3bcba387f278a21a80d60ad81fb000fba24
Juan Carlos Gonzalez <javascriptdeveloper17@gmail.com>
2018-08-08 13:19:04 -0400

    added a few new methods and created some test data. may have to steer away from objects, or add support for data properties that are objects

--- data-structures/linked-list/LinkedList.js
+++ data-structures/linked-list/LinkedList.js
@@ -4,67 +4,122 @@ const Node = require('./LinkedListNode');
 
 function LinkedList() {
   this.head = null;
-  this.tail = null;
+  // this.tail = null;
+}
 
-  /**
-   * @returns {void}
-   * @param {*} data
-   * @name append
-   */
-  this.append = function(data) {
-    if (this.head == null) {
-      this.head = new Node(data);
+/**
+ * @returns {void}
+ * @param {*} data
+ * @name append
+ */
+LinkedList.prototype.append = function(data) {
+  if (this.head == null) {
+    this.head = new Node(data);
+    return;
+  }
+
+  let current = this.head;
+  while (current.next !== null) {
+    current = current.next;
+  }
+
+  current.next = new Node(data);
+}
+
+/**
+ * @returns {void}
+ * @param {*} data
+ * @name prepend
+ */
+LinkedList.prototype.prepend = function(data) {
+  let newHead = new Node(data);
+  newHead.next = this.head;
+  this.head = newHead;
+}
+
+/**
+ * @returns {void}
+ * @param {*} data
+ * @name deleteNodeWithValue
+ */
+LinkedList.prototype.deleteNodeWithValue = function(data) {
+  if (this.head == null) return;
+  // If the Node we want to delete is the Head...
+  if (this.head.data == data) {
+    // Cut it out of the LinkedList
+    this.head = this.head.next;
+  }
+
+  // Walk through the LinkedList
+  // starting at the Head value
+  let current = this.head;
+  while (current.next !== null) {
+    // If the next Node is the one we want
+    // to delete, skip it.
+    if (current.next.data == data) {
+      current.next = current.next.next;
       return;
     }
 
-    let current = this.head;
-    while (current.next !== null) {
-      current = current.next;
-    }
+    // Move on to the next element
+    current = current.next;
+  }
+}
 
-    current.next = new Node(data);
+/**
+ * @returns {LinkedListNode || null}
+ * @param {*} data
+ * @name findNode
+ */
+LinkedList.prototype.findNode = function(data) {
+  // If List is empty, return
+  if (this.head == null) return;
+  
+  // If the head node is the node
+  // we're looking for, return it
+  if (this.head.data == data) {
+    return this.head;
   }
 
-  /**
-   * @returns {void}
-   * @param {*} data
-   * @name prepend
-   */
-  this.prepend = function(data) {
-    let newHead = new Node(data);
-    newHead.next = this.head;
-    this.head = newHead;
-  }
+  // Otherwise, walk through
+  // the linked list and find
+  // the node we're looking for
+  let current = this.head;
+  while(current.next !== null) {
 
-  /**
-   * @returns {void}
-   * @param {*} data
-   * @name deleteNodeWithValue
-   */
-  this.deleteNodeWithValue = function(data) {
-    if (this.head == null) return;
-    // If the Node we want to delete is the Head...
-    if (this.head.data == data) {
-      // Cut it out of the LinkedList
-      this.head = this.head.next;
+    // If the next node is the one
+    // we're looking for, return it
+    if(current.next.data == data) {
+      return current.next.data
     }
 
-    // Walk through the LinkedList
-    // starting at the Head value
-    let current = this.head;
-    while (current.next !== null) {
-      // If the next Node is the one we want
-      // to delete, skip it.
-      if (current.next.data == data) {
-        current.next = current.next.next;
-        return;
-      }
-
-      // Move on to the next element
-      current = current.next;
-    }
+    current = current.next;
   }
 
+  // If we don't find such a node,
+  // return null
+  return null;
+
+}
+
+/**
+ * @returns {Number}
+ * @name size
+ * @description
+ * Counts the # of Nodes in the
+ * list and returns that count
+ */
+LinkedList.prototype.size = function() {
+  if (this.head == null) return;
+
+  let current = this.head;
+  let count = 1;
+  while(current.next !== null) {
+    count++;
+    current = current.next;
+  }
+
+  return count;
 }
 
 module.exports = LinkedList;
--- data-structures/linked-list/index.js
+++ data-structures/linked-list/index.js
@@ -1,11 +1,42 @@
 var LinkedList = require('./LinkedList');
 var Node = require('./LinkedListNode');
 
+function Individual(name, rel) {
+  this.name = name;
+  this.rel = rel;
+};
+
 var myList = new LinkedList();
-var myNode = new Node("Juan Carlos");
+var family = [
+  new Individual("Juan Carlos", "self"),
+  new Individual("Carissa", "wife"),
 
-myList.append(myNode);
-// myList.append(myNode);
+  new Individual("Charlie", "bf"),
+  new Individual("Juanjo", "bf2"),
+  new Individual("Fernando", "bf3"),
 
+  new Individual("Raul", "grandfather"),
+  new Individual("Carolina", "grandmother"),
+
+  new Individual("Eugenia", "grandmother"),
+  
+  new Individual("Juan Carlos", "father"),
+  new Individual("Sandra", "mother"),
+  new Individual("Andrea", "sister"),
+
+  new Individual("Sergio", "cousin"),
+  new Individual("Nicole", "cousin"),
+  new Individual("Karla", "cousin"),
+
+  new Individual("Daniel", "cousin"),
+  new Individual("Nicholas", "cousin"),
+  new Individual("Erika", "cousin"),
+
+  new Individual("Isabel", "cousin"),
+  new Individual("Juliana", "cousin"),
+];
+
+for (let member of family)
+  myList.append(member);
 
 console.log(myList);
